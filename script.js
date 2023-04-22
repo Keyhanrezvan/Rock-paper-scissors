1c2cd348f7abbf1e356dc9f8ee630d84b6956399
Keyhanrezvan <Keyhanrezvan@gmail.com>
2023-04-22 15:39:05 -0500

    working RPS

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ function getComputerChoice(){
 
    let num = Math.floor(Math.random() * 3)
    let result = computerChoices[num]
-console.log(result)
    return result
-
 }
+
+let playerScore = 0
+let computerScore =0
 
 function playRound(playerSelection, computerSelection){
 
@@ -21,13 +22,32 @@ function playRound(playerSelection, computerSelection){
    if (lowPlayer === computerSelection){
       return `Its a tie! You both chose ${lowPlayer}`
    } else if ((lowPlayer === 'rock' && computerSelection === 'scissors') || (lowPlayer === 'scissors' && computerSelection === 'paper') || (lowPlayer === 'paper' && computerSelection === 'rock')) {
+      playerScore ++
       return `You win! ${lowPlayer} beats ${computerSelection}`
    } else {
+      computerScore ++
       return `You lose! ${computerSelection} beats ${lowPlayer}`
    }
+  
 }
 
+const playerSelection = "paper";
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+
+ while (computerScore < 5 && playerScore < 5){
+   
+   console.log(playRound(playerSelection, getComputerChoice()))
+   
+ }
+
+ if (computerScore === 5) {
+   return "You lost the game to a computer!"
+ } else if (computerScore === playerScore){
+   return "Its a tie!"
+ } else { return "You win the game!!"}
+
+}
+
+console.log(game())
